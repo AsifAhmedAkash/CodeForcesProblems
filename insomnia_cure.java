d3230bb834527fcb5fd39c926a7f4f6d32281dda
Asif Ahmed <asifahmedakash32@gmail.com>
2026-02-13 12:47:17 +0600

    insomnia cure

---
+++ insomnia_cure.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class insomnia_cure {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] torcers = new int[4]; // array of size 4
+        int suffered_drags = 0;
+
+        for (int i = 0; i < 4; i++) {
+            torcers[i] = sc.nextInt(); // read each integer
+        }
+
+        int d = sc.nextInt();
+
+        for (int i = 1; i <= d; i++) {
+            for (int j = 0; j < torcers.length; j++) {
+                if (i % torcers[j] == 0) {
+                    suffered_drags++;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(suffered_drags);
+    }
+}
